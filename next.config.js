ef40f9b78fa04fa0209bb32547aa7344f9e70e9f
Atomic996 <hchamadi425@gmail.com>
2026-04-18 18:11:25 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  webpack: (config) => {
-    config.experiments = { ...config.experiments, asyncWebAssembly: true };
-    return config;
-  },
-};
-module.exports = nextConfig;
